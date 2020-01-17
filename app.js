c3190149059337277eb89cf3a609a909debfec4b
Jake Simmens <rocknjake@gmail.com>
2020-01-17 08:11:44 -0600

    Added authentication feature and pages.  Adjusted navbar to reflect when user is logged in.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express    = require("express"),
-      app        = express(),
-      bodyParser = require("body-parser"),
-      mongoose   = require("mongoose"),
-      Campground = require("./models/campground"),
-      Comment    = require("./models/comment"),
-      seedDB     = require("./seeds");
+const express               = require("express"),
+      app                   = express(),
+      bodyParser            = require("body-parser"),
+      mongoose              = require("mongoose"),
+      passport              = require("passport"),
+      LocalStrategy         = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose"),
+      User                  = require("./models/user",)
+      Campground            = require("./models/campground"),
+      Comment               = require("./models/comment"),
+      seedDB                = require("./seeds");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -14,6 +18,24 @@ mongoose.set("useUnifiedTopology", true);
 mongoose.connect("mongodb://localhost/yelp_camp");
 
 seedDB();
+
+//Passport configuration
+app.use(require("express-session")({
+    secret: "Rise of Skywalker is better than Last Jedi",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//middleware to pass on current user data
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -85,8 +107,8 @@ app.get("/campgrounds/:id", (req, res) => {
 //  COMMENTS ROUTES
 //  ********************
 
-//NEW COMMENT
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+//NEW COMMENT, only access if logged in
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, foundCampground) => {
         if(err){
             console.log(err);
@@ -98,7 +120,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
 });
 
 //CREATE COMMENT
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
 
     Campground.findById(req.params.id, (err, foundCampground) => {
         if(err){
@@ -123,7 +145,64 @@ app.post("/campgrounds/:id/comments", (req, res) => {
     const author = req
 });
 
+//###################
+//Authorization Routes
+//###################
 
+//Show register form
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+//handle user sign up
+app.post("/register", (req, res) => {
+    var newUser = new User({username: req.body.username});
+
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+
+//###################
+//Login Routes
+//###################
+
+//Show login
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }),
+    (req, res) => {}
+);
+
+//###################
+//Logout Route
+//###################
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if( req.isAuthenticated() ){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, () => {
     console.log("YelpCamp server on 3000");
